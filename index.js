dfcbba630ab2c74c91323bb47bb9d579e9a1bcf2
Rakibur Rahaman <mail.rakiburrahaman@gmail.com>
2023-09-12 04:47:46 +0600

    mmmmmmmmmmmmmm

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Bot, GrammyError, HttpError } = require("grammy");
 const { autoQuote } = require("@roziscoding/grammy-autoquote");
 const fs = require("fs");
 const path = require("path");
+const { NlpManager } = require("node-nlp");
 
 if (fs.existsSync(".env")) {
   require("dotenv").config();
@@ -11,6 +12,18 @@ const botToken = process.env.BOT_TOKEN;
 if (!botToken) {
   throw new Error("BOT_TOKEN is not set in environment variables! Exiting...");
 }
+
+// Create an instance of NlpManager
+const nlpManager = new NlpManager({ languages: ["en"] });
+
+// Define intents and their training phrases
+nlpManager.addDocument("en", "What is your name?", "bot.name");
+nlpManager.addDocument("en", "Who created you?", "bot.creator");
+nlpManager.addDocument("en", "What is the weather like today?", "weather.today");
+nlpManager.addDocument("en", "Tell me a joke", "bot.joke");
+
+// Train the NLP manager
+nlpManager.train();
 
 async function start() {
   const bot = new Bot(botToken);
@@ -39,6 +52,35 @@ async function start() {
   bot.command("start", (ctx) =>
     ctx.reply("Hello!\n\n" + "Run the /help command to see what I can do!")
   );
+
+  // Handle incoming messages
+  bot.on("text", async (ctx) => {
+    const { text } = ctx.message;
+    
+    // Use the NLP manager to recognize intents
+    const response = await nlpManager.process("en", text);
+
+    // Check the recognized intent and respond accordingly
+    switch (response.intent) {
+      case "bot.name":
+        ctx.reply("My name is Telegram Bot!");
+        break;
+      case "bot.creator":
+        ctx.reply("I was created by a developer.");
+        break;
+      case "weather.today":
+        // Implement weather fetching logic here
+        ctx.reply("I don't know the weather right now.");
+        break;
+      case "bot.joke":
+        // Implement joke retrieval logic here
+        ctx.reply("Why did the chicken cross the road? To get to the other side!");
+        break;
+      default:
+        // Handle unrecognized intent
+        ctx.reply("I'm not sure how to respond to that.");
+    }
+  });
 
   bot.catch((err) => {
     const ctx = err.ctx;
